f9b4b22009b56b845f4ca339d941e284e8c66bc1
Greg Connell <greg.connell@ff0000.com>
2018-01-04 12:23:23 -0800

    adds prettierrc

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-const debug = require('debug');
-var log = debug('wp-plugin-watch-offset');
+const debug = require('debug')
+var log = debug('wp-plugin-watch-offset')
 
-
-function WpPluginWatchOffset() {};
-
+function WpPluginWatchOffset() {}
 
 WpPluginWatchOffset.prototype.apply = function(compiler) {
-	const timefix = 11000;
+	const timefix = 11000
 
 	// offset start-time
 	compiler.plugin('watch-run', (watching, callback) => {
-		log(`advance start-time by ${timefix/1000}s - avoids generated files triggering watch cycle`);
-		watching.startTime += timefix;
-		callback();
-	});
+		log(`advance start-time by ${timefix / 1000}s - avoids generated files triggering watch cycle`)
+		watching.startTime += timefix
+		callback()
+	})
 
 	// restore start-time
-	compiler.plugin('done', (stats) => {
-		log(`restore start-time`);
-		stats.startTime -= timefix;
-	});
-};
+	compiler.plugin('done', stats => {
+		log(`restore start-time`)
+		stats.startTime -= timefix
+	})
+}
 
-
-module.exports = WpPluginWatchOffset;
+module.exports = WpPluginWatchOffset
